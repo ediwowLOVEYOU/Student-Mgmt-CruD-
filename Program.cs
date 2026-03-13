105e4cb91ce6f04b9dd3d7514f032ad72c1b0ae5
ediwowLOVEYOU <59858593+ediwowLOVEYOU@users.noreply.github.com>
2026-03-13 22:47:39 +0800

    remove

--- Program.cs
+++ Program.cs
@@ -1,101 +1,105 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using ModelLayer;
+using ServiceLayer;
 namespace Student_Mgmt
 {
     internal class Program
     {
-       
-
+        static StudentManager Manager = new StudentManager();
+        static bool Exits = true;
         static void Main(string[] args)
         {
-            static List<string> StudentData = new List<string>();
-
-            Console.WriteLine("Welcome to Student Management System");
-            Console.WriteLine("Please select an option:");
-            Console.WriteLine("1.Create Student Information");
-            Console.WriteLine("2.Retrieve Student Information");
-            Console.WriteLine("3.Update Studen information");
-            Console.WriteLine("4.Delete Student Information");
-            Console.WriteLine("5.Exit");
-
-
-            Console.WriteLine("Create Student Information");
-            Console.WriteLine("1.Student Name");
-            Console.WriteLine("2.Student Age");
-            Console.WriteLine("3.Student No");
-            Console.WriteLine("4.Exit");
-            int choice = Convert.ToInt32(Console.ReadLine());
-
-            switch (choice)
+      
+            while (Exits)
             {
-                case 1:
-                    CreateInfo();
-                    break;
-                case 2:
-                    RetrieveInfo();
-                    break;
-                case 3:
-                    UpdateInfo();
-                    break;
-                case 4:
-                    DelInfo();
-                    break;
-                case 5:
-                    Exit();
-                    break;
 
+                Console.WriteLine("Welcome to Student Management System");
+                Console.WriteLine("Please select an option:");
+                Console.WriteLine("1.Create Student Information");
+                Console.WriteLine("2.Retrieve Student Information");
+                Console.WriteLine("3.Update Studen information");
+                Console.WriteLine("4.Delete Student Information");
+                Console.WriteLine("5.Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        CreateInfo();
+                        break;
+                    case 2:
+                        RetrieveInfo();
+                        break;
+                    case 3:
+                        UpdateInfo();
+                        break;
+                    case 4:
+                        DelInfo();
+                        break;
+                    case 5:
+                        Exit();
+                        break;
+
+                }
+
+
+                static void CreateInfo()
+                {
+
+                    Console.Write("Enter Name:");
+                    string Name = Console.ReadLine() ?? string.Empty;
+                    Console.WriteLine("Age:");
+                    int Age = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Student No:");
+                    int IDNo = Convert.ToInt32(Console.ReadLine());
+
+                    Student_Data student = new Student_Data
+                    {
+                        StudentNoModel = IDNo,
+                        NameModel = Name,
+                        AgeModel = Age,
+                    };
+                    Manager.Add(student);
+                    Console.WriteLine("Succes Student Added");
+                }
+                static void RetrieveInfo()
+                {
+                    Console.WriteLine("Student Information:");
+                    var Students = Manager.GetAllStudents();
+                    foreach (var student in Students)
+                    {
+                        Console.WriteLine($"ID: {student.StudentNoModel} Name: {student.NameModel} Age: {student.AgeModel}");
+                    }
+                }
+                static void UpdateInfo()
+                {
+
+                    /* if(StudentNo != Student_Data)
+                     {
+                         Console.WriteLine("NOT VALID INPUT:" StudentNo);
+                     }
+                     else if (StudentNO == Student_Data) {
+                     Console.Write("Enter Students Number");
+                     int StudentNo = Convert.ToInt32(Console.ReadLine());
+
+                     Student_Data.Add(StudentNo);
+         */
+                    Console.WriteLine("Updated the Info");
+
+                }
+                static void DelInfo()
+                {
+                    Console.WriteLine("Delete Section");
+
+
+                }
+                static void Exit()
+                {
+                   Exits = false;
+                    Console.WriteLine("Exit Section");
+                }
             }
-
-
-            static void CreateInfo() {
-
-                StudentData Student = new StudentData();
-
-            Console.Write("Enter Name:");
-            Student.Name = Console.ReadLine();
-            Console.WriteLine("Age:");
-           Student.Age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Student No:");
-            Student.StudentNo = Convert.ToInt32(Console.ReadLine());
-
-            StudentData.Add(Name);
-                StudentData.Add(Age);
-                StudentData.Add(StudentNo);
-        }
-        static void RetrieveInfo()  
-        {
-            Console.WriteLine("Student Information:");
-            foreach (string Student in StudentData)
-            {
-                Console.WriteLine(Student);
-            }
-        }
-        static void UpdateInfo()
-        {
-
-            if(StudentNo != StudentData)
-            {
-                Console.WriteLine("NOT VALID INPUT:" StudentNo);
-            }
-            else if (StudentNO == StudentData) {
-            Console.Write("Enter Students Number");
-            int StudentNo = Convert.ToInt32(Console.ReadLine());
-
-            StudentData.Add(StudentNo);
-
-            Console.WriteLine("Updated the Info");
-
-        }   
-        static void DelInfo()
-        {
-
-
-
-        }
-        static void Exit()
-        {
-
         }
     }
 }
